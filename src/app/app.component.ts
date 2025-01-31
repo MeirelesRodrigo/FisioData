abdeabb7ea2e5fcaa56e3f7f59aeb2a7d9dca0c5
Rodrigo Meireles <meireles.rsilva22@gmail.com>
2025-01-31 15:23:30 -0300

    Create a side-bar menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconsServicesService } from './services/icons-service/icons-services.service'
+import { SideBarComponent } from "./components/side-bar/side-bar.component";
+import { GenericFormComponent } from "./components/generic-form/generic-form.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule, SideBarComponent, GenericFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FioData';
+
+  constructor(
+    //REMOVER POSTERIORMENTE
+    public iconsService : IconsServicesService
+  ){}
 }
