e67a220605c5889d638aca446b4c96830d2594cd
Rodrigo Meireles <meireles.rsilva22@gmail.com>
2025-02-03 11:57:45 -0300

    page New-Pacient-Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconsServicesService } from './services/icons-service/icons-services.service'
-import { SideBarComponent } from "./components/side-bar/side-bar.component";
 import { GenericFormComponent } from "./components/generic-form/generic-form.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FontAwesomeModule, SideBarComponent, GenericFormComponent],
+  imports: [RouterOutlet, FontAwesomeModule, GenericFormComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
